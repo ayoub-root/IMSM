2f224c9a42c20adb3ee5b45e48e39ac2271d7d05
THE ROOT <a.benayache@univ-batna2.dz>
2019-10-30 02:10:33 +0100

    call blockly framework

--- IMSMAPPS/static/msmst/blockly.js
+++ IMSMAPPS/static/msmst/blockly.js
@@ -1,14 +1,15 @@
 
- alert("ffff  ")
+ //alert("ffff  ")
             function device(name,bg) {
 
   Blockly.Blocks[name] = {
   init: function() {
-    this.appendStatementInput("NAME")
-        .setCheck(null);
+
     this.appendDummyInput()
         .setAlign(Blockly.ALIGN_RIGHT)
         .appendField(new Blockly.FieldImage(bg, 115, 115, { alt: "*", flipRtl: "FALSE" }));
+    this.appendStatementInput("NAME")
+        .setCheck(null);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
@@ -21,14 +22,13 @@
         function component(name,bg) {
 
   Blockly.Blocks[name] = {
+
   init: function() {
-    this.appendStatementInput("NAME")
-        .setCheck(null);
     this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(new Blockly.FieldImage(bg, 115, 115, { alt: "*", flipRtl: "FALSE" }));
+        .setAlign(Blockly.ALIGN_CENTRE)
+        .appendField(new Blockly.FieldImage(bg, 115, 115, { alt: "*", flipRtl: "TRUE"}));
     this.setPreviousStatement(false, null);
-    this.setNextStatement(false, null);
+    this.setNextStatement(true, null);
     this.setColour(230);
  this.setTooltip("");
  this.setHelpUrl("");
@@ -38,18 +38,20 @@
 }          //    alert("{{ post.name }}");
   function microservice(name) {
 
-  Blockly.Blocks[name] = {
+Blockly.Blocks[name] = {
   init: function() {
-    this.appendStatementInput("NAME")
-        .setCheck(null);
-
-    this.setPreviousStatement(false, null);
-    this.setNextStatement(false, null);
+    this.appendValueInput("NAME")
+        .setAlign(Blockly.ALIGN_CENTRE)
+        .appendField(name);
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
     this.setColour(230);
  this.setTooltip("");
  this.setHelpUrl("");
   }
 };
+
 
 }
   function data(name) {
