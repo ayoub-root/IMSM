b0abd9161f50a7e0d5f9a5025a22bbbc19e6337b
THE ROOT <a.benayache@univ-batna2.dz>
2019-11-18 03:39:00 +0100

    call blockly framework

--- IMSMAPPS/views.py
+++ IMSMAPPS/views.py
@@ -275,7 +275,7 @@ def runtime(request):
     return render(request,'IMSMAPPS/myapp.html',data)
 def pymicroservice(*argv, **kwargs):
     print("run service : "+str(argv[0])+' with option : '+str(kwargs['args']))
-ssoijzzu
+ssoijzzuju
 def storage(request):
     data={'data':request}
     print(request.encoding)
