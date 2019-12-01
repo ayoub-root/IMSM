c83feda0a69ca8031a1ab6ca848372e80d7152d1
THE ROOT <a.benayache@univ-batna2.dz>
2019-12-01 03:39:13 +0100

    call blockly framework

--- IMSM/settings.py
+++ IMSM/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'CHANGEME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
+DEBUG = True
 ALLOWED_HOSTS = []
 
 
--- IMSMAPPS/s.py
+++ IMSMAPPS/s.py
@@ -1,22 +1,3 @@
-import pandas as pd
-import numpy as np
-
-
-
-def find_mean_age(Sex,Typ):
-    return temp.loc[(temp['Sex']==Sex)&(temp['Typ']==Typ),'Age']
-
-if __name__ =='__main__':
-    df = pd.DataFrame({'Id':[1,2,3,4,5,6],
-                   'Sex':['male','male','female','male','female','female'],
-                   'Age':[21,float('NAN'),float('NAN'),23,56,32],
-                   'Typ':['A','A','V','V','V','V']})
-    print(df)
-
-    temp = df.loc[(df['Age'].notnull())&(df['Age'] < 65 ),
-                ['Age','Sex','Typ']].groupby(['Sex','Typ'],as_index=False).mean()
-
-    df.loc[df['Age'].isnull(), ['Age']] = df.apply(lambda row: find_mean_age(row['Sex'], row['Typ'][0]),
-                                                   axis=1)
-
-    print(df)
+txt="123.45.67"
+newvar= txt.split(".")
+print(newvar)
